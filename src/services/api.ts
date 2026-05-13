007382145c0698e65f95bf246627b93b93444512
Aniket Jaiswal <dongon40413@gmail.com>
2026-05-13 20:35:38 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.MODE === 'production' ? 'https://college-event-api-obst.onrender.com/api' : '/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
